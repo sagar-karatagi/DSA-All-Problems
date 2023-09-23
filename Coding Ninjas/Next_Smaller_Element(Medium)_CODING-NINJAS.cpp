fddab8b55d3bf58039296d6c3ce45edcd179bbac
sagar-karatagi <sagrkaratagi@gmail.com>
2023-09-23 22:00:33 +0530

    Add files via upload

---
+++ Coding Ninjas/Next_Smaller_Element(Medium)_CODING-NINJAS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n){ 
+    vector<int> ans(n);
+    stack<int> s;
+    s.push(-1);
+
+    for(int i = n-1;i>-1;i--){
+        while(s.top()>=arr[i]){
+            s.pop();
+        }
+        ans[i]=s.top();
+        s.push(arr[i]);
+    }
+
+    return ans;
+}
