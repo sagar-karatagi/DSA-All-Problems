8ae78e38e78232d3ba007b8e6b9b826060aa8c52
sagar-karatagi <sagrkaratagi@gmail.com>
2025-07-13 09:42:39 +0530

    Create 2410_Maximum Matching of Players With Trainers.cpp

---
+++ LeetCode/2410_Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& p, vector<int>& t) {
+        sort(p.begin(),p.end());
+        sort(t.begin(),t.end());
+
+        int i = 0,j = 0;
+        int cnt = 0;
+        while(i<p.size()&&j<t.size()){
+
+            if(p[i]<=t[j]){
+                i++;
+                j++;
+                ++cnt;
+            }else{
+                j++;
+            }
+        }
+        return cnt;
+    }
+};
